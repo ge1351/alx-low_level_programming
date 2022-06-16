dbc0047351c7cf4f9ec3804a134e028e8088c386
ge1351 <alxmaty@gmail.com>
2022-06-16 04:22:53 -0700

    main.2 git commit -m main.2

--- 0x06-pointers_arrays_strings/mains/main.2.c
+++ 0x06-pointers_arrays_strings/mains/main.2.c
@@ -1,31 +1,41 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+ /**
+  * main - check the code
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-    char *s = "heo";
-    char *f;
-
-    f = _strchr(s, 'l');
-    printf("%s\n", f);
-    return (0);
+  char s1[98];
+  char *p;
+  int i;
+  
+  for (i = 0; i < 98 - 1; i++)
+    {
+      s1[i] = '*'; 
+    }
+  s1[i] = '\0';
+  printf("%s\n", s1);
+  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+  printf("%s\n", s1);
+  printf("%s\n", p);
+  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+  printf("%s", s1);
+  printf("%s", p);
+  for (i = 0; i < 98; i++)
+    {
+      if (i % 10)
+        {
+	  printf(" "); 
+        }
+      if (!(i % 10) && i)
+        {
+	  printf("\n"); 
+        }
+      printf("0x%02x", s1[i]); 
+    } 
+  printf("\n");
+   return (0);
+ 
 }
-
-/*
-Write a function that locates a character in a string.
-
-    Prototype: char *_strchr(char *s, char c);
-    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
-
-FYI: The standard library provides a similar function: strchr. Run man strchr to learn more.
-
-Sample output:
-
-llo
-
-*/
