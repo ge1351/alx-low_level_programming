06904fa69d2eeefca1027fc2ec9c6be14769e6c9
ge1351 <alxmaty@gmail.com>
2022-06-29 02:42:29 -0700

    statick librery

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+char *_strcat(char *dest, char *src)
+{
+			return (0);
+}
+
+                               1-isdigit.c
+
+#include "main.h"
+			       int _isdigit(int c)
+{
+			return (0);
+}
