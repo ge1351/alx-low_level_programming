ad46c9f55e58777463e2c0e58d67f97dd90e1562
ge1351 <alxmaty@gmail.com>
2022-06-29 06:32:51 -0700

    statick librery

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,31 @@
 #include "main.h"
+
+/**
+ *
+ *  * _isupper - checks for uppercase character
+ *
+ *   *
+ *
+ *    * @c: parameter hard-coded in main
+ *
+ *     *
+ *
+ *      * Return: 0 or 1
+ *
+ *       */
+
 int _isupper(int c)
 {
-			return (0);
+		int i;
+
+			if (c >= 'A' && c <= 'Z')
+					{
+								i = 1;
+										return (i);
+											}
+				else
+						{
+									i = 0;
+											return (i);
+												}
 }
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *  * _isdigit - checks for a digit (0 through 9)
+ *
+ *   *
+ *
+ *    * @c: parameter hard-coded in main
+ *
+ *     *
+ *
+ *      * Return: 1 or 0
+ *
+ *       */
+
+int _isdigit(int c)
+{
+		in i;
+
+			if (c >= '0' && c <= '9')
+					{
+								i = 1;
+									}
+				else
+						{
+									i = 0;
+										}
+				return (i);
+}
+
