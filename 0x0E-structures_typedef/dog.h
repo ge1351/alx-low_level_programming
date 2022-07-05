b7c8d1c22bbd8dca8a3115142329fc5ca9d7f10c
ge1351 <alxmaty@gmail.com>
2022-07-05 12:05:39 -0700

    nn

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- *
- *  * struct dog - dog data
- *
- *   * @name: name of the dog
- *
- *    * @age: age of the dog
- *
- *     * @owner: name of the owner of the dog
- *
- *      *
- *
- *       * Description: Defines metadata about the dog and type of dog's struct
- *
- *        */
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
 typedef struct dog
 {
-              	char *name;
+        	char *name;
 		float age;
-		car *owner;
+		char *owner;
 } dog_t;
 /* Additional function prototypes */
 int _putchar(char c);
@@ -29,4 +20,3 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
-
