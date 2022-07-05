96e314b94033bfc3491fc24f86e0cfd1865f98ee
ge1351 <alxmaty@gmail.com>
2022-07-05 12:08:37 -0700

    mm

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -5,30 +5,31 @@
 
 
 /**
-
- * main - check the code for Holberton School students.
-
  *
-
- * Return: Always 0.
-
- */
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
 int main(void)
 
 {
 
-    struct dog my_dog;
+	    struct dog my_dog;
 
 
 
-    my_dog.name = "Django";
+	        my_dog.name = "Poppy";
 
-    my_dog.age = 3.5;
+		    my_dog.age = 3.5;
 
-    my_dog.owner = "Jay";
+		        my_dog.owner = "Bob";
 
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+			    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 
-    return (0);
+			        return (0);
 
+}
