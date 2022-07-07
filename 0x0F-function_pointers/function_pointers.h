b79710996b3931906ab8ae3ba09b6db0722c3907
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-07 22:21:18 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,18 @@
-#ifndef FILE_F_POINTRS
-#define FILE_F_POINTRS
+#ifndef VARIADIC_FNS
+#define VARIADIC_FNS
 
-#include <stdlib.h>
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
 
-void print_name(char *name, void (*f)(char *));
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
 
-void array_iterator(int *array, size_t size, void (*action)(int));
-
-int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
