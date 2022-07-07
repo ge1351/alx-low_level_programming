7f1f907cbcadd0e420e4d656e1a298e34e0bed5b
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-07 21:58:34 +0300

    Update 1-main.c

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-	print_numbers(", ", 4, 0, 98, -1024, 402);
-	return (0);
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
 }
 Footer
 © 2022 GitHub, Inc.
-Footer navigation
-Terms
