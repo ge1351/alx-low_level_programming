6696a3960d8be2e551d6c3f45c217338c9feabe6
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-08 23:08:14 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,18 +1,18 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-
-typedef struct validTypes
-{
-	char *valid;
-	void (*f)();
-} v_types;
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
