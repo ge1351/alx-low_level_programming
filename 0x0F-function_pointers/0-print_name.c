408a1fe1c55036672f7acb8faa27d69482427560
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-08 22:57:01 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,18 @@
-#include "function_pointers.h"
-#include <stdlib.h>
 /**
- * print_name - prints the name
- * @name: name
- * @f: function
+ * print_name - program startup
+ * @name: char being evaluated
+ * @f: function being evaluated
+(*
+ * Description: prints a name)?
+ * Return: return (0) is the required function signature
  */
+
+#include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-   if (name == NULL || f == NULL)
-           return;
-   f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
 
