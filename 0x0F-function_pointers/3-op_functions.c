2968a3463913f854055232c9efbd91747ece2584
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-08 23:04:13 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
- * op_add - addition
- * @a: sumnd 1
- * @b: sumnd 2
- * Return: rsult
+ * op_add - program startup
+ * @a: first int  being evaluated
+ * @b: second int  being evaluated
+(*
+ * Description: returns the sum of a and b)?
+ * Return: return (0) is the required function signature
  */
 
 int op_add(int a, int b)
@@ -14,10 +21,12 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - subtraction
- * @a: minuend
- * @b: sustrnd
- * Return: result
+ * op_sub - program startup
+ * @a: first int  being evaluated
+ * @b: second int  being evaluated
+(*
+ * Description: returns the difference of a and b)?
+ * Return: return (0) is the required function signature
  */
 
 int op_sub(int a, int b)
@@ -26,12 +35,46 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiplication
- * @a: multp 1
- * @b: multp 2
- * Return: result
+ * op_mul - program startup
+ * @a: first int  being evaluated
+ * @b: second int  being evaluated
+(*
+ * Description: returns the product of a and b)?
+ * Return: return (0) is the required function signature
  */
 
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - program startup
+ * @a: first int  being evaluated
+ * @b: second int  being evaluated
+(*
+ * Description: returns the result of the division of a by b)?
+ * Return: return (0) is the required function signature
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - program startup
+ * @a: first int  being evaluated
+ * @b: second int  being evaluated
+(*
+ * Description: returns the remainder of the division of a by b)?
+ * Return: return (0) is the required function signature
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
 int op_mul(int a, int b)
 {
 	return (a * b);
