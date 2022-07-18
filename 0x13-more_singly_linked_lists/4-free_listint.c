9d221dc12243ac299f68a2342895bf61ad222a54
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-18 13:19:10 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * free_listint -  function that frees a listint_t list.
+ * free_listint - frees a listint list
  *
- * @head: Parameter that point to the head of the linked list
+ * @head: head of list to free
+ *
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *next;
 
-	while (head)
+	if (head == NULL)
+		return;
+
+	for (next = head->next; head != NULL; head = next)
 	{
-		temp = head->next;
+		next = head->next;
 		free(head);
-		head = temp;
 	}
 }
