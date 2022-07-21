c41720f348ea4685e906eafe6173ace09bfc5b78
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-21 15:18:23 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,11 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _BIT_MANIP_
+#define _BIT_MANIP_
 
-#include <stdio.h>
-#include <string.h>
-
+int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
+void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
