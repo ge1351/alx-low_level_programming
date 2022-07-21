a0ab76ce8f1dab9618ba42c7237b907117f30fbe
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-21 16:01:16 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
-  * clear_bit - Sets the value of a bit to 0 at a given index
-  * @n: The number to modify
-  * @index: The index in the number to modify
-  *
-  * Return: 1 if it worked, or -1 if an error occurred
-  */
+ * clear_bit - set a bit to 0
+ * @n: the number
+ * @index: the position to set the bit
+ * Description: set 1 bit to 0
+ * section header: the header of this function is holberton.h
+ * Return: 1 in success -1 in fail
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < sizeof(unsigned long int) * 8)
-	{
-		*n &= (~(1 << index));
-		return (1);
-	}
-
-	return (-1);
+	if (index > 64)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
