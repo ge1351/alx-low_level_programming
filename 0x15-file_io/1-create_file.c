1f6d1156e26b2de3b723c7c0092061b750a8b654
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-25 17:41:15 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,40 @@
 #include "main.h"
-
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - function that creates a file
+ * permission rw-------
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
  *
- * Return: 1 if it success. -1 if it fails.
+ * @filename: name of the file to create
+ * if filename is NULL return -1
+ * @text_content: NULL terminated string to write to the file
+ * if text_content is NULL create an empty file
+ *
+ * Return: 1 on succes
+ * -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, num_written, len = 0;
 
-	if (!filename)
+	if (filename == 0)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		num_written = write(fd, text_content, len);
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
+		if (num_written == -1)
+			return (-1);
+	}
 	close(fd);
-
 	return (1);
 }
