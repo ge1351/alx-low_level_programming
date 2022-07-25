2bd47f6c584a1c3c38c6a2924b43be9ac1d074e7
ge1351 <105641499+ge1351@users.noreply.github.com>
2022-07-25 17:43:35 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,20 +1,23 @@
 #include "main.h"
-
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * append_text_to_file - function that appends text at the end of a file
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to append to end of file
+ *
+ * Return: Returns: 1 on success, -1 on failure
+ * Do not create the file if it does not exit
+ *
+ * if filename is NULL return -1
+ * If text_content is NULL, do not add anything to the file.
+ * Return 1 if the file exists and -1 if the file does not exist or if you
+ * do not have the required permissions to write the file
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, n_print, len = 0;
 
-	if (!filename)
+	if (filename == 0)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
@@ -24,16 +27,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
+		while (text_content[len])
+			len++;
+		n_print = write(fd, text_content, len);
+		if (n_print == -1)
 			return (-1);
 	}
-
 	close(fd);
-
 	return (1);
 }
